1bf7e20ca8e5f4b4f403d160ac4b014bf515d93d
Abhinay Jangde <abhinayjangde@gmail.com>
2023-12-11 17:02:26 +0530

    everything is ok in root file which is index.js

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 import express from "express"
+import db from "./middlewares/db.js"
+import dotenv from "dotenv"
+import web from "./routes/web.js"
+dotenv.config()
 const app = express()
 const port = process.env.PORT || '3000'
 const host = process.env.HOST || '127.0.0.1'
+
+db(process.env.DB_URL)
+
+app.use('/api/student',web)
+
 app.get('/',(req,res)=>{
     res.status(200).send({"status":"success"})
 })
